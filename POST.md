651312239d3c80d69e692b0183c047ad6e53df3a
Daniel Zello <75264908+Ravost99@users.noreply.github.com>
2022-05-04 16:05:19 +0000

    ez bugfix

---
+++ POST.md
@@ -0,0 +1,11 @@
+#ghostping #pingfinder #python3 #discordbot #discord
+
+## A simple and useful ghost ping finder
+
+![img](https://ravost.repl.co/uploads/pings.png)
+
+A very simple solution: **Ghost Pinger**
+
+[Invite if u don't want to read.](https://discord.com/oauth2/authorize?client_id=968995551678980126&permissions=412317199424&scope=bot)
+
+Read More in [README.md](https://replit.com/@Ravost/Ghost-Pinger?v=1#README.md)
--- main.py
+++ main.py
@@ -13,6 +13,9 @@ os.system('clear')
 
 intents = disnake.Intents.default()
 bot = commands.Bot(commands.when_mentioned_or('>>'), intents=intents)
+whitelisted_ids = [742835236315856907] #whitelist ids here
+mod_logs = 971101865263640616 # mod logs channel id
+
 bot.remove_command('help')
 
 @bot.event
@@ -26,36 +29,58 @@ async def on_ready():
 async def on_message(message):
     await bot.process_commands(message)
 
-
 @bot.event
 async def on_message_delete(message):
     now = datetime.now()
     current_time = now.strftime("%I:%M:%S %p")
     current_date = datetime.today().strftime('%m-%d-%Y')
-
+    mod_ch = await bot.fetch_channel(mod_logs)
     msg = message.content
+    
     if '@' in msg:
-      embed = disnake.Embed(
-        title='Ghost Ping Found!',
-        color=0xFF0000
-      )
-      embed.add_field(
-        name='Author',
-        value=message.author,
-        inline=True
-      )
-      embed.add_field(
-        name='Message:',
-        value=msg,
-        inline=True
-      )
-      embed.set_footer(
-        text=f'{current_time} {current_date}'
-      )
-      print(f'Ghost Ping Found! By {message.author}, message: {msg}. ({current_time} {current_date})\n')
-      time.sleep(1)
-      await message.channel.send(embed=embed)
-
+      if message.author.bot or message.author.id in whitelisted_ids:
+        print(f"Ignoring ghost ping by {message.author}")
+        embed = disnake.Embed(
+          title="Ignoring Ghost Ping",
+          description=f"by {message.author}",
+          color=0xFF0000
+        )
+        embed.add_field(
+          name='Author',
+          value=message.author,
+          inline=True
+        )
+        embed.add_field(
+          name='Message:',
+          value=msg,
+          inline=True
+        )
+        embed.set_footer(
+          text=f'{current_time} {current_date}'
+        )
+        await mod_ch.send(embed=embed)
+      else:
+        embed = disnake.Embed(
+          title='Ghost Ping Found!',
+          color=0xFF0000
+        )
+        embed.add_field(
+          name='Author',
+          value=message.author,
+          inline=True
+        )
+        embed.add_field(
+          name='Message:',
+          value=msg,
+          inline=True
+        )
+        embed.set_footer(
+          text=f'{current_time} {current_date}'
+        )
+        print(f'Ghost Ping Found! By {message.author}, message: {msg}. ({current_time} {current_date})\n')
+        time.sleep(1)
+        await message.channel.send(embed=embed)
+        await mod_ch.send(embed=embed)
 
 @bot.command(name='ping', description='Check if the bot is alive.')
 async def ping(ctx):
